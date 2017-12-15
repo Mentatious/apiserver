f504d5ab3c9e4a798ddbe1247553740d0b43975d
Alex Ermolov <aermolov@mirantis.com>
2017-12-16 00:12:03 +0300

    flag: migrated to "kingpin" library

--- apiserver.go
+++ apiserver.go
@@ -6,8 +6,8 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/gorilla/rpc/v2"
 	"github.com/gorilla/rpc/v2/json2"
-	"github.com/simonleung8/flags"
 	"github.com/wiedzmin/goodies"
+	"gopkg.in/alecthomas/kingpin.v2"
 	"net/http"
 	"os"
 	"os/signal"
@@ -17,25 +17,22 @@ import (
 func main() {
 	_, log := goodies.InitLogging(false, false)
 
-	fc := flags.New()
-	fc.NewStringFlag("host", "h", "host to listen")
-	fc.NewStringFlag("port", "p", "port to listen")
-	fc.NewStringFlag("dbhost", "d", "DB host")
-	fc.Parse(os.Args...)
+	app := kingpin.New("mentat-apiserver", "Mentat API server")
+	Host := app.Flag("host", "Host to listen").Short('h').String()
+	Port := app.Flag("port", "Port to listen").Short('p').Required().String()
+	DBHost := app.Flag("dbhost", "DB host").Short('d').Required().String()
 
-	host := fc.String("host")
-	port := fc.String("port")
-	dbhost := fc.String("dbhost")
+	kingpin.MustParse(app.Parse(os.Args[1:]))
 
-	if port == "" {
+	if *Port == "" {
 		log.Infof("No port to listen, exiting....")
 		os.Exit(1)
-	} else if dbhost == "" {
+	} else if *DBHost == "" {
 		log.Infof("No dbhost to connect, exiting....")
 		os.Exit(1)
 	}
 
-	bindAddress := fmt.Sprintf("%s:%s", host, port)
+	bindAddress := fmt.Sprintf("%s:%s", *Host, *Port)
 	log.Infof("listening on %s", bindAddress)
 
 	rpcServer := rpc.NewServer()
@@ -44,7 +41,7 @@ func main() {
 	rpcServer.RegisterCodec(json2.NewCodec(), "application/json;charset=UTF-8")
 
 	entryAPI := new(entry.Service)
-	entryAPI.Init("entry", dbhost, log)
+	entryAPI.Init("entry", *DBHost, log)
 	defer entryAPI.Destroy()
 
 	c := make(chan os.Signal, 2)
